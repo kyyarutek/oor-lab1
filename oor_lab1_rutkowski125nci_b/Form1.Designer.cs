80500e2e670d4bacfc721ac65addfa6fa9f0d110
kyyarutek <kyyarutek@gmail.com>
2016-01-25 07:36:03 +0100

    jeden

---
+++ oor_lab1_rutkowski125nci_b/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace oor_lab1_rutkowski125nci_b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(342, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "URUCHOM KOLEJNY WĄTEK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "10000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Do ilu liczyć?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(368, 101);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sebastian Rutkowski 125NCI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ oor_lab1_rutkowski125nci_b/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace oor_lab1_rutkowski125nci_b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AllocConsole();
+                }
+        int number = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Thread thr = new Thread(doSomething);
+            Thread thr2 = new Thread(doSomething);
+            Thread thr3 = new Thread(doSomething);
+            Thread thr4 = new Thread(doSomething);
+            if (thr.ThreadState != System.Threading.ThreadState.Running)
+            {
+                thr.Start();
+                number++;
+            }
+            else if (thr.ThreadState == System.Threading.ThreadState.Running)
+            {
+                thr2.Join();
+                number++;
+
+            }
+            else if (thr2.ThreadState == System.Threading.ThreadState.Running && thr.ThreadState == System.Threading.ThreadState.Running)
+            {
+                thr3.Join();
+                number++;
+
+            }
+            else if (thr4.ThreadState == System.Threading.ThreadState.Running && thr2.ThreadState == System.Threading.ThreadState.Running && thr.ThreadState == System.Threading.ThreadState.Running)
+            {
+                thr4.Join();
+                number++;
+
+            }
+        }
+        private void doSomething()
+        {
+            Int32 licz_do = Convert.ToInt32(textBox1.Text);
+            if (number <= 1)
+            {
+                for (int i = 0; i <= licz_do; i++)
+                    Console.WriteLine(" [1] " + i);
+                number--;
+            }
+           
+
+            else if (number == 2)
+            {
+                for (int i2 = 0; i2 <= licz_do; i2++)
+                    Console.WriteLine(" [2] " + i2);
+                number--;
+
+            }
+            else if (number == 3)
+            {
+                for (int i3 = 0; i3 <= licz_do; i3++)
+                    Console.WriteLine(" [3] " + i3);
+                number--;
+
+            }
+            else if (number == 4)
+            {
+                for (int i4 = 0; i4 <= licz_do; i4++)
+                    Console.WriteLine(" [4] " + i4);
+                number--;
+
+            }
+            else if (number > 4)
+            {
+            }
+
+
+        }
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+
+    }
+
+
+}
